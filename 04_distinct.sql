1ec0c2a7347cdc70bb5bf0316b2f44f88843b83e
jyyyyy4 <annajy1425@gmail.com>
2026-03-09 16:33:56 +0900

    4.5

---
+++ 04_distinct.sql
@@ -0,0 +1,31 @@
+-- 04_distinct : 중복 컬럼 값 제거 후 조회
+
+-- 메뉴 테이블 내에 존재하는 카테고리의 종류 조회
+SELECT
+    DISTINCT category_code
+FROM
+    tbl_menu
+ORDER BY
+    category_code;
+
+-- NULL 값이 존재하는 컬럼에서 사용할 경우 => null 값도 중복 제거 되어 1행만 남음
+SELECT 
+    DISTINCT ref_category_code
+FROM
+    tbl_category
+ORDER BY
+    1; -- select 절에 나열된 컬럼 순번으로도 정렬 가능
+
+-- [주의] 다중열에 distinct 사용할 경우
+-- 다중열의 좋바 결과 행의 중복이 제거된다.
+SELECT
+    DISTINCT category_code,
+    orderable_status
+FROM
+    tbl_menu;
+
+SELECT
+    category_code,
+    orderable_status
+FROM
+    tbl_menu;
